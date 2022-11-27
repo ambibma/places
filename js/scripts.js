925432ce492646fa7b7049e29fccadf94adb52aa
ambi hidalgo <ambio.pk@gmail.com>
2022-11-27 14:37:09 -0800

    add handle form sub, event listener

--- js/scripts.js
+++ js/scripts.js
@@ -25,9 +25,26 @@ Travelogue.prototype.deleteLocation = function(id) {
 // Location Business Logic
 function Location(city, landmarks, restaurants, climate) {
   this.city = city;
-  this.landmarks = landmarks;
   this.restaurants = restaurants;
+  this.landmarks = landmarks;
   this.climate = climate;
 }
 
 // UI logic
+let travelogue = new Travelogue();
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const inputtedCity = document.querySelector("input#new-city").value
+  const inputtedRestaurants = document.querySelector("input#new-restaurants").value;
+  const inputtedLandmarks = document.querySelector("input#new-landmarks").value;
+  const inputtedClimate = document.querySelector("input#new-climate").value;
+  let newLocation = new Location(inputtedCity, inputtedRestaurants, inputtedLandmarks, inputtedClimate);
+  travelogue.addLocation(newLocation);
+  console.log(travelogue.locations);
+
+}
+  window.addEventListener("load", function (){
+    document.querySelector("form#new-location").addEventListener("submit", handleFormSubmission);
+  });
+
